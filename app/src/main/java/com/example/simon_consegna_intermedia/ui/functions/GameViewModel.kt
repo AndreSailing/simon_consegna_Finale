b762980c430e9eaa14cae0b56f9b217afb95b91b
AndreSailing <andrea.miolato@studenti.unipd.it>
2026-05-31 04:33:50 +0200

    Database XD

--- app/src/main/java/com/example/simon_consegna_intermedia/ui/functions/GameViewModel.kt
+++ app/src/main/java/com/example/simon_consegna_intermedia/ui/functions/GameViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.simon_consegna_intermedia.ui.functions
 
-import android.R
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
