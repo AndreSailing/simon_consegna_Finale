9bc2e27d37183ae3e28848ebbd9ec9239169e736
AndreSailing <andrea.miolato@studenti.unipd.it>
2026-05-31 23:57:33 +0200

    DettaglioPArtita string

--- app/src/main/java/com/example/simon_consegna_intermedia/ui/DettaglioPartitaActyvity/DettaglioPartitaActivity.kt
+++ app/src/main/java/com/example/simon_consegna_intermedia/ui/DettaglioPartitaActyvity/DettaglioPartitaActivity.kt
@@ -1,9 +1,9 @@
 package com.example.simon_consegna_intermedia.ui.DettaglioPartitaActyvity
 
 import PartitaObject
-import android.content.Intent
+
 import android.os.Bundle
-import android.os.PersistableBundle
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,8 +11,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.simon_consegna_intermedia.ui.components.Screen
 import com.example.simon_consegna_intermedia.ui.components.ScreenDettaglioPartita
 import com.example.simon_consegna_intermedia.ui.theme.Simon_Consegna_IntermediaTheme
 
